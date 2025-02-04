8898fcb10b528611833f35ccb975638853120a47
Vladyslav <finyagin07@gmail.com>
2025-02-04 16:26:50 +0200

    lab3.1

---
+++ src/mypackage/MainMethod.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class MainMethod {
+    public static void main(String[] args) {
+        One str1 = new One(14,21,27.2);
+        Two str2 = new Two(34,44,52.7);
+        Three str3 = new Three(24,70,23.1);
+
+        str1.result();
+        str2.result();
+        str3.result();
+    }
+}
---
+++ src/mypackage/One.java
@@ -0,0 +1,20 @@
+package mypackage;
+
+public class One {
+    //змінні екземплярів класу
+    int object1;
+    int object2;
+    double object3;
+
+    //конструктор
+    public One(int object1, int object2, double object3){
+        this.object1=object1;
+        this.object2=object2;
+        this.object3=object3;
+    }
+
+    public void result() {
+        System.out.println("FirstClass - Object1: " + object1 + ", Object2: " + object2 + ", Object3: " + object3);
+    }
+
+}
