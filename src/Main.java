f44cb6b0a0af2ba74f5ca69acdd28849e4756e27
Vladyslav <finyagin07@gmail.com>
2025-03-04 01:12:52 +0200

    lab2.2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and adaadadadweldzfzdnzdnzcome!");
+        Scanner scanner = new Scanner(System.in);
+
+        //завдання 1
+        System.out.print("Введіть число для перевірки діапазону: ");
+        int n = scanner.nextInt();
+        if (n > 50 && n < 100) {
+            System.out.println("Число " + n + " міститься в проміжку (50; 100)");
+        } else {
+            System.out.println("Число " + n + " не міститься в проміжку (50; 100)");
+        }
+
+        //завдання 2
+        System.out.print("Введіть тризначне число: ");
+        int num = scanner.nextInt();
+        int maxDigit = Math.max(num / 100, Math.max((num / 10) % 10, num % 10));
+        System.out.println("Найбільша цифра: " + maxDigit);
+
+        //завдання 3
+        System.out.print("Введіть поточний поверх: ");
+        int currentFloor = scanner.nextInt();
+        System.out.print("Введіть бажаний поверх: ");
+        int targetFloor = scanner.nextInt();
+        if (targetFloor == 2) {
+            targetFloor = (currentFloor < 2) ? 3 : 1;
+        }
+        System.out.println("Ви " + (targetFloor > currentFloor ? "піднялись" : "спустились") + " на " + targetFloor + " поверх");
+
+        //завдання 4
+        System.out.print("Введіть команду (Так, ОК, Yes, Y, +, Ok, Ні, NO, N, -, No): ");
+        String command = scanner.next();
+        switch (command) {
+            case "Так", "ОК", "Yes", "Y", "+", "Ok" -> System.out.println("Я погоджуюсь!");
+            case "Ні", "NO", "N", "-", "No" -> System.out.println("Я відмовляюсь!");
+            default -> System.out.println("Невідома команда!");
+        }
+
+        //завдання 5
+        System.out.print("Введіть три різні числа: ");
+        int a = scanner.nextInt(), b = scanner.nextInt(), c = scanner.nextInt();
+        if (a > b) { int temp = a; a = b; b = temp; }
+        if (b > c) { int temp = b; b = c; c = temp; }
+        if (a > b) { int temp = a; a = b; b = temp; }
+        System.out.println("Числа у порядку зростання: " + a + " " + b + " " + c);
     }
 }
