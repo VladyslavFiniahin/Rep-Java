dae88e2e45b800db0739efb0df63a10732dcb000
Vladyslav <finyagin07@gmail.com>
2025-04-21 23:19:28 +0300

    lab11

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class SeatUnavailableException extends Exception {
+    public SeatUnavailableException(String message) {
+        super(message);
+    }
+}
+
+public class Main {
+    static boolean[][] seats = new boolean[8][11];
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.println("\n Бронювання місця в кінотеатрі");
+                System.out.print("Введіть номер ряду (0–7): ");
+                int row = Integer.parseInt(scanner.nextLine());
+
+                System.out.print("Введіть номер місця (0–10): ");
+                int seat = Integer.parseInt(scanner.nextLine());
+
+                bookSeat(row, seat);
+                System.out.println("Місце [" + row + "][" + seat + "] заброньовано");
+
+            } catch (SeatUnavailableException e) {
+                System.out.println("Помилка: " + e.getMessage());
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Місце не знайдено");
+            } catch (NumberFormatException e) {
+                System.out.println("Введіть число");
+            }
+
+            System.out.print("\n Продовжити бронювання? (так/ні): ");
+            String choice = scanner.nextLine().trim().toLowerCase();
+            if (!choice.equals("так")) {
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static void bookSeat(int row, int seat) throws SeatUnavailableException {
+        if (seats[row][seat]) {
+            throw new SeatUnavailableException("Місце зайняте");
+        } else {
+            seats[row][seat] = true;
         }
     }
 }
